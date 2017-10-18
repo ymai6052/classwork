a3997a94bdb27a23a592c46addb9a501e2f49268
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-10-18 13:09:52 -0400

    with levin template

---
+++ classwork/src/shapes/Rectangle.java
@@ -0,0 +1,34 @@
+package shapes;
+
+/**
+ * @author Mr Levin Created 10/16/2017 Lab 2.1 shapes
+ *
+ */
+public class Rectangle implements Shape {
+
+	private int length;
+	private int width;
+
+	public Rectangle(int length, int width) {
+		this.length = length;
+		this.width = width;
+	}
+
+
+	public double calculateArea() {
+		// To be written by student
+		return 0.0;
+	}
+
+
+	public double calculatePerimeter() {
+		// To be written by student
+		return 0.0;
+	}
+
+
+	public String toString() {
+		return "Rectangle Width: " + width + " Length: " + length + " Area: " + this.calculateArea() + " Perimeter: "
+				+ this.calculatePerimeter();
+	}
+}
---
+++ classwork/src/shapes/Square.java
@@ -0,0 +1,18 @@
+package shapes;
+
+public class Square extends Rectangle {
+
+	int side;
+
+	public Square(int side) {
+		super(side, side);
+		this.side = side;
+	}
+
+	@Override
+	public String toString() {
+		// To be written by student
+		return "";
+	}
+
+}
