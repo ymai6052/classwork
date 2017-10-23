979f1ffb0a5c3fcf1a0871d9ccb61ab8f1ece623
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-10-23 13:13:48 -0400

    doubletest

---
+++ classwork/src/doubled/doubletest.java
@@ -0,0 +1,35 @@
+package doubled;
+
+public class doubletest {
+	public static void main(String[]arges)
+	{
+		int x=2;
+		String[] tempArr= {"milk","cake","candy"};
+		System.out.println(doubledint(x));
+		doubleArr(tempArr);
+		for(int i=0;i<tempArr.length;i++)
+		{
+			System.out.print(tempArr[i]);
+		}
+	}
+	public static int doubledint(int x)
+	{
+		x=x+x;
+		return x;
+	}
+	public static String doubledstr(String str1)
+	{
+		str1=str1+str1;
+		return str1;
+	}
+	public static String[] doubleArr(String[] strArr)
+	{
+		int x=0;
+		while(x<strArr.length)
+		{
+			strArr[x]=strArr[x]+strArr[x];
+			x++;
+		}
+		return strArr;
+	}
+}
