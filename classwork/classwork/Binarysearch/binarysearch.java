974b19fbb9679e6a9f01c01ff5c60abea7aa33e4
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-06 13:16:59 -0500

    binary

---
+++ classwork/classwork/Binarysearch/binarysearch.java
@@ -0,0 +1,16 @@
+package Binarysearch;
+
+public class binarysearch {
+	public static void main(String[]args)
+	{
+		
+	}
+	public static boolean binsearch(int[]numbers,int num)
+	{
+		int x=numbers.length-1;
+		int counter=0;
+		while(x>0)
+			if
+		return true;
+	}
+}
