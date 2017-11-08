96e65987f4398682d21f3343b338c27245db1bdd
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-08 12:09:41 -0500

    copyarray

---
+++ classwork/classwork/copy/copyarray.java
@@ -0,0 +1,37 @@
+package copy;
+
+public class copyarray {
+	public static void main(String[]args)
+	{
+		int[] intArray=new int[10];
+		
+		for(int i=0;i<intArray.length;i++)
+			intArray[i]=(int)(Math.random()*10);
+		
+		int[] copy=copyArray(intArray);
+		
+		copy[copy.length-1]=-1;
+		
+		System.out.println("Array 1: ");
+		printArray(intArray);
+		
+		System.out.println("Copy of Array 1:");
+		printArray(copy);
+	}
+	public static int[] copyArray(int[] intArray)
+	{
+		int[] copy=new int[intArray.length];
+		
+		for(int i=0;i<intArray.length;i++)
+			copy[i]=intArray[i];
+		
+		return copy;
+	}
+	public static void printArray(int[] arr)
+	{
+		for(int i=0;i<arr.length;i++)
+			System.out.print("["+arr[i]+"]");
+		
+		System.out.println();;
+	}
+}
