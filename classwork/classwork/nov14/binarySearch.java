0583415b57bdff459c9760d6b057c2614db9a642
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-11-14 12:10:44 -0500

    binarysearch

---
+++ classwork/classwork/nov14/binarySearch.java
@@ -0,0 +1,33 @@
+package nov14;
+
+public class binarySearch {
+	public static void main(String[]args)
+	{
+	int[] test1= {1,2,3,4,5,6,7,8,9,10};
+	int[] test2= {0,4,5,6,12,15,17,23,29};
+	
+	System.out.println(binarySearch(test1,3,0,test1.length));
+	System.out.println(binarySearch(test2,23,0,test1.length));
+	System.out.println(binarySearch(test1,3,0,test1.length));
+	System.out.println(binarySearch(test2,25,0,test1.length));
+	}
+	public static int binarySearch(int[] nums,int query,int first, int last)
+	{
+		if(last>=first)
+		{
+			int index=(first+last)/2;
+			int guess=nums[index];
+			if(guess==query)
+			{
+				return index;
+			}
+			if(guess>query)
+			{
+				return binarySearch(nums,query,first,index-1);
+			}
+			return binarySearch(nums,query,index+1,last);
+		}
+		return -1;
+	}
+
+}
