b0d64127524a524b6677ba6744dcfc8436ba4fe4
BT_1N3_29 <BT_1N3_29@BT_1N3_29.bktech.local>
2017-12-19 12:58:34 -0500

    sdaggadgdfgaghdf

---
+++ csv/src/Runner/utilities.java
@@ -0,0 +1,32 @@
+package Runner;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+import csvfile.Book;
+public abstract class utilities {
+	ArrayList<String> CSVData;
+	int numColumns;
+	public CSVUtilities(File csv)
+	{
+		
+	}
+	public List<String> getColumnHeaders()
+	{
+		
+	}
+	public List<String> getData(int column)
+	{
+		
+	}
+	public List<Double> getData(int column)
+	{
+		
+	}
+}
